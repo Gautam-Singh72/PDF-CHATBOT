e0b2a9160b941a8cf69dd89d1d796cb5b19c1b43
Gautam Singh <gautamsinghthakur.2004@gmail.com>
2026-09-22 10:53:13 +0530

    created a function to get user input

--- app.py
+++ app.py
@@ -57,3 +57,25 @@ def get_conversational_chain(model_name, vectorstore=None, api_key=None):
         model= ChatGoogleGenerativeAI(model="gemini-3.5-flash", temperature=0.3, google_api_key=api_key)
         prompt=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
         chain=load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+# take user input
+def user_input(user_question, model_name, api_key, pdf_docs, conversation_history):
+    if api_key is None or pdf_docs is None:
+        st.warning("please upload any pdf and valid api key")
+        return
+    text_chunks=get_text_chunks(get_pdf_text(pdf_docs), model_name)
+    vector_store=get_vectore_store(text_chunks, model_name, api_key)
+    user_question_output=""
+    response_output=""
+    if model_name=="Google AI":
+        embeddings=GoogleGenerativeAIEmbeddings(model='model/embedding-001', google_api_key=api_key)
+        new_db=FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+        docs=new_db.similarity_search(user_question)
+        chain=get_conversational_chain("Google AI", vectorstore=new_db, api_key=api_key)
+        response=chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+        user_question_output=user_question
+        response_output=response['output_text']
+        pdf_names=[pdf.name for pdf in pdf_docs] if pdf_docs else []
+        conversation_history.append((user_question_output, response_output, model_name, datetime.now().strftime
+        ('%Y-%m-%d %H:%M:%S'),",".join(pdf_names)))
+        
